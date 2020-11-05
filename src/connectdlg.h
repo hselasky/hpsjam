858517541c2f4306d32823b74fb72978af4b0805
Hans Petter Selasky <hps@selasky.org>
2020-11-05 12:23:31 +0100

    hpsjam: Set default server location to 127.0.0.1.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/connectdlg.h
+++ src/connectdlg.h
@@ -83,6 +83,7 @@ class HpsJamConnectServer : public QGroupBox {
 public:
 	HpsJamConnectServer() : gl(this) {
 		setTitle(tr("Select server"));
+		edit.setText(QString("127.0.0.1"));
 		gl.addWidget(&edit, 0,0);
 		gl.addWidget(&list, 1,0);
 		gl.setRowStretch(1,1);
