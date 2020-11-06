a781fa500b05c3bd2b4073ddb5e1f347a53af73d
Hans Petter Selasky <hps@selasky.org>
2020-11-06 15:10:06 +0100

    hpsjam: Complete implementation of lyrics dialog.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/lyricsdlg.cpp
+++ src/lyricsdlg.cpp
@@ -23,4 +23,145 @@
  * SUCH DAMAGE.
  */
 
+#include <QKeyEvent>
+#include <QFontDialog>
+#include <QPainter>
+
+#include "hpsjam.h"
+#include "clientdlg.h"
 #include "lyricsdlg.h"
+
+void
+HpsJamLyrics :: paintEvent(QPaintEvent *event)
+{
+	QPainter paint(this);
+	int w = width();
+	int h = height();
+
+	QColor bg(0,0,0);
+	QColor fg(255,255,255);
+
+	paint.setRenderHints(QPainter::Antialiasing, 1);
+	paint.fillRect(QRectF(0,0,w,h), bg);
+
+	if (customFont == false)
+		font.setPixelSize(w / 16);
+
+	float y_pos = 0;
+
+	for (uint8_t x = 0; x != maxIndex; x++) {
+		HpsJamLyricsAnim &aobj = anim[(index + x) % maxIndex];
+
+		if (aobj.isVisible() == false)
+			continue;
+
+		if (aobj.ypos_curr == 0.0f)
+			aobj.ypos_curr = y_pos;
+		else
+			y_pos = aobj.ypos_curr;
+
+		paint.setFont(font);
+
+		float wf = font.pixelSize();
+		int flags = Qt::TextWordWrap | Qt::TextDontClip | Qt::AlignTop;
+
+		QRectF txtBound;
+		QRectF txtMax(0,0,w,h);
+
+		paint.drawText(txtMax, Qt::AlignLeft |
+		    Qt::TextDontPrint | flags, aobj.str, &txtBound);
+		txtMax.setHeight(txtBound.height() + wf);
+
+		flags |= Qt::AlignHCenter | Qt::AlignVCenter;
+
+		/* offset bounding box */
+		txtMax.adjust(
+		    aobj.xpos_curr,
+		    aobj.ypos_curr + wf / 2.0f,
+		    aobj.xpos_curr,
+		    aobj.ypos_curr + wf / 2.0f);
+
+		/* draw text */
+		paint.setPen(fg);
+		paint.setBrush(fg);
+		paint.setOpacity(aobj.opacity_curr);
+		paint.drawText(txtMax, flags, aobj.str);
+
+		/* store height and width */
+		aobj.height = txtMax.height();
+		aobj.width = txtMax.width();
+
+		/* get next position */
+		y_pos += aobj.height;
+	}
+}
+
+void
+HpsJamLyrics :: keyPressEvent(QKeyEvent *key)
+{
+	if (key->key() == Qt::Key_Escape) {
+		if (hpsjam_client->windowState() & Qt::WindowFullScreen)
+			handle_fullscreen();
+	}
+}
+
+void
+HpsJamLyrics :: mouseDoubleClickEvent(QMouseEvent *e)
+{
+	handle_fullscreen();
+}
+
+void
+HpsJamLyrics :: handle_fullscreen()
+{
+	hpsjam_client->setWindowState(
+	    hpsjam_client->windowState() ^ Qt::WindowFullScreen);
+}
+
+void
+HpsJamLyrics :: handle_watchdog()
+{
+	for (uint8_t x = 0; x != maxIndex; x++) {
+		if (anim[x].step())
+			update();
+	}
+}
+
+void
+HpsJamLyrics :: handle_font_dialog()
+{
+	bool success;
+
+	QFont temp = QFontDialog::getFont(&success, font, this);
+
+	if (success) {
+		temp.setPixelSize(QFontInfo(temp).pixelSize());
+		font = temp;
+		customFont = true;
+		update();
+	}
+}
+
+void
+HpsJamLyrics :: append(const QString &str)
+{
+	const uint8_t n = (index + 1) % maxIndex;
+	const uint8_t p = (maxIndex + index - 1) % maxIndex;
+
+	anim[index].reset();
+	anim[index].str = str;
+	anim[index].fadeIn();
+	anim[index].ypos_curr = anim[p].ypos_curr + anim[p].height;
+
+	const float shift = anim[n].ypos_curr + anim[n].height;
+
+	/* shift rest of text blocks up */
+	for (uint8_t x = 0; x != maxIndex; x++)
+		anim[x].moveUp(shift);
+
+	/* advance index */
+	index = (index + 1) % maxIndex;
+
+	if (!isVisible())
+		hpsjam_client->b_lyrics.setFlashing();
+}
--- src/lyricsdlg.h
+++ src/lyricsdlg.h
@@ -26,10 +26,117 @@
 #ifndef _HPSJAM_LYRICSDLG_H_
 #define	_HPSJAM_LYRICSDLG_H_
 
+#include <stdbool.h>
+
+#include <QTimer>
 #include <QWidget>
+#include <QString>
+#include <QFont>
+
+#define	HPSJAM_TRAN_MAX 5
+
+class HpsJamLyricsAnim {
+public:
+	QString str;
+	float opacity_curr;
+	float opacity_step;
+	float xpos_curr;
+	float xpos_step;
+	float ypos_curr;
+	float ypos_step;
+	float width;
+	float height;
+	uint8_t currStep;
+
+	void reset()
+	{
+		opacity_curr = 0.0f;
+		opacity_step = 0.0f;
+		xpos_curr = 0.0f;
+		xpos_step = 0.0f;
+		ypos_curr = 0.0f;
+		ypos_step = 0.0f;
+		width = 0.0f;
+		height = 0.0f;
+		currStep = 0;
+		str = QString();
+	}
+	HpsJamLyricsAnim()
+	{
+		reset();
+		currStep = HPSJAM_TRAN_MAX;
+	}
+	bool step()
+	{
+		if (currStep >= HPSJAM_TRAN_MAX) {
+			/* animation finished */
+			opacity_step = 0;
+			xpos_step = 0;
+			ypos_step = 0;
+			currStep++;
+			return (false);
+		}
+		/* animation in progress */
+		opacity_curr += opacity_step;
+		xpos_curr += xpos_step;
+		ypos_curr += ypos_step;
+		currStep++;
+		return (true);
+	};
+	void fadeOut()
+	{
+		opacity_step = -(opacity_curr / HPSJAM_TRAN_MAX);
+		currStep = 0;
+	}
+	void fadeIn()
+	{
+		opacity_step = ((1.0 - opacity_curr) / HPSJAM_TRAN_MAX);
+		currStep = 0;
+	}
+	void moveUp(float pix)
+	{
+		if (pix != 0.0f) {
+			ypos_step = -(pix / HPSJAM_TRAN_MAX);
+			currStep = 0;
+		}
+	}
+	bool isVisible()
+	{
+		return ((ypos_curr + height) >= 0.0f &&
+		    (opacity_curr >= (1.0 / 1024.0)));
+	}
+	bool isAnimating()
+	{
+		return (currStep <= HPSJAM_TRAN_MAX);
+	}
+};
 
 class HpsJamLyrics : public QWidget {
+	Q_OBJECT;
+public:
+	enum { maxIndex = 3 };
 
+	HpsJamLyrics() {
+		index = 0;
+		customFont = false;
+		connect(&watchdog, SIGNAL(timeout()), this, SLOT(handle_watchdog()));
+		watchdog.start(1000 / (3 * HPSJAM_TRAN_MAX));
+	};
+	uint8_t index;
+	HpsJamLyricsAnim anim[maxIndex];
+	bool customFont;
+	QFont font;
+	QTimer watchdog;
+
+	void paintEvent(QPaintEvent *);
+	void keyPressEvent(QKeyEvent *);
+	void mouseDoubleClickEvent(QMouseEvent *);
+	void append(const QString &);
+
+public slots:
+	void handle_fullscreen();
+	void handle_watchdog();
+	void handle_font_dialog();
 };
 
 #endif		/* _HPSJAM_LYRICSDLG_H_ */
