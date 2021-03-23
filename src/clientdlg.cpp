c697dc39747cdd2b631dabaf2c4e6cf719b43d4d
Hans Petter Selasky <hps@selasky.org>
2021-03-23 23:52:10 +0100

    hpsjam: Correctly remember last connected server.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/clientdlg.cpp
+++ src/clientdlg.cpp
@@ -247,7 +247,8 @@ HpsJamClient :: loadSettings()
 	w_connect->icon.setSelection(settings.value("connect/icon", QString("0")).toInt());
 	w_connect->password.edit.setText(settings.value("connect/password", QString()).toString());
 	w_connect->server.edit.setText(settings.value("connect/server", QString("127.0.0.1:" HPSJAM_DEFAULT_PORT_STR)).toString());
-	w_connect->server.list.setPlainText(settings.value("connect/list", QString()).toString());
+	HPSJAM_NO_SIGNAL(w_connect->server.list,setPlainText(settings.value("connect/list", QString()).toString()));
+	w_connect->server.list.updateSelection(w_connect->server.edit.text());
 
 	HPSJAM_NO_SIGNAL(w_config->effects,setIndex(settings.value("config/effects_level", QString("0")).toInt()));
 	w_config->up_fmt.setIndex(settings.value("config/uplink_format", QString("1")).toInt());
