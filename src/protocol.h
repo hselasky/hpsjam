06675bfd09630bee9b6abd89e19c26aaee9d66cd
Hans Petter Selasky <hps@selasky.org>
2022-09-22 10:54:26 +0200

    hpsjam: Improve check for out-of-order packets.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/protocol.h
+++ src/protocol.h
@@ -591,6 +591,11 @@ top:
 
 	void receive(const union hpsjam_frame &frame) {
 		const uint8_t rx_seqno = frame.hdr.getSequence();
+		unsigned delta = (HPSJAM_SEQ_MAX + rx_seqno - (unsigned)last_seqno) % HPSJAM_SEQ_MAX;
+
+		/* check for out-of-order packet */
+		if (delta >= (HPSJAM_SEQ_MAX / 2))
+			return;
 
 		current[rx_seqno] = frame;
 		valid[rx_seqno] = HPSJAM_MASK_VALID;
