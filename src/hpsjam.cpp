998fe9e52588b4b403097523ace94a34b5946d84
Hans Petter Selasky <hps@selasky.org>
2022-10-10 11:47:54 +0200

    hpsjam: Add support for simple application debug console over TCP.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/hpsjam.cpp
+++ src/hpsjam.cpp
@@ -113,6 +113,39 @@ static const struct option hpsjam_opts[] = {
 	{ NULL, 0, NULL, 0 }
 };
 
+#ifdef HAVE_DEBUG_CONSOLE
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+static void
+debug_console(uint32_t dest, uint16_t port)
+{
+	int s = ::socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s < 0)
+		return;
+
+	struct sockaddr_in sa = {
+		.sin_family = AF_INET,
+		.sin_len = sizeof(sa),
+		.sin_port = htons(port),
+		.sin_addr.s_addr = htonl(dest),
+	};
+	if (::connect(s, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+		return;
+
+	int flag = 1;
+	::setsockopt(s, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
+	::setbuf(stderr, NULL);
+	::setbuf(stdout, NULL);
+	::dup2(s, 1);
+	::dup2(s, 2);
+	::close(s);
+}
+#endif
+
 static void
 usage(void)
 {
